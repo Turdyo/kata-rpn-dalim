dea8e1a4561b40eb6ab41eb967f899cffcf08e00
AbdullatifGhajar <abdullatif.ghajar@gmail.com>
2023-11-08 10:19:45 +0100

    Add tests for /

--- test/rpn.test.js
+++ test/rpn.test.js
@@ -28,8 +28,19 @@ describe('RPN', function () {
                 expect(evaluateRPN('9 11 12 * *')).to.equal(1188);
             });
         });
+        describe('should work with /', function () {
+            it('should work with two numbers', function () {
+                expect(evaluateRPN('99 9 /')).to.equal(11);
+            });
+            it('should work with more than two numbers', function () {
+                expect(evaluateRPN('120 2 6 / /')).to.equal(10);
+            });
+            it('should return int if result is float', function () {
+                expect(evaluateRPN('9 2 /')).to.be.an(4);
+            });
+        });
     });
-    
+
     describe('edge cases', function () {
         it('some test', function () {
             expect(1).to.equal(1);
