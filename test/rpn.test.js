7c18f27f2a1d9c43219f33cf38f1c8cce2313ec4
AbdullatifGhajar <abdullatif.ghajar@gmail.com>
2023-11-08 10:24:04 +0100

    Expect only strings as output

--- test/rpn.test.js
+++ test/rpn.test.js
@@ -6,37 +6,37 @@ describe('RPN', function () {
     describe('normal cases', function () {
         describe('should work with +', function () {
             it('should work with two numbers', function () {
-                expect(evaluateRPN('9 11 +')).to.equal(20);
+                expect(evaluateRPN('9 11 +')).to.equal('20');
             });
             it('should work with more than two numbers', function () {
-                expect(evaluateRPN('9 11 12 + +')).to.equal(32);
+                expect(evaluateRPN('9 11 12 + +')).to.equal('32');
             });
         });
         describe('should work with -', function () {
             it('should work with two numbers', function () {
-                expect(evaluateRPN('9 11 -')).to.equal(-2);
+                expect(evaluateRPN('9 11 -')).to.equal('-2');
             });
             it('should work with more than two numbers', function () {
-                expect(evaluateRPN('9 11 12 - -')).to.equal(-14);
+                expect(evaluateRPN('9 11 12 - -')).to.equal('-14');
             });
         });
         describe('should work with *', function () {
             it('should work with two numbers', function () {
-                expect(evaluateRPN('9 11 *')).to.equal(99);
+                expect(evaluateRPN('9 11 *')).to.equal('99');
             });
             it('should work with more than two numbers', function () {
-                expect(evaluateRPN('9 11 12 * *')).to.equal(1188);
+                expect(evaluateRPN('9 11 12 * *')).to.equal('1188');
             });
         });
         describe('should work with /', function () {
             it('should work with two numbers', function () {
-                expect(evaluateRPN('99 9 /')).to.equal(11);
+                expect(evaluateRPN('99 9 /')).to.equal('11');
             });
             it('should work with more than two numbers', function () {
-                expect(evaluateRPN('120 2 6 / /')).to.equal(10);
+                expect(evaluateRPN('120 2 6 / /')).to.equal('10');
             });
             it('should return int if result is float', function () {
-                expect(evaluateRPN('9 2 /')).to.be.an(4);
+                expect(evaluateRPN('9 2 /')).to.be.an('4');
             });
             it('should return error if divide by 0', function () {
                 expect(evaluateRPN('9 0 /')).to.be.an('Error: division by zero');
