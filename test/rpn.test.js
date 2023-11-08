575e2f20cbfba91a19e14bef3a286a4e8ea6ed6c
AbdullatifGhajar <abdullatif.ghajar@gmail.com>
2023-11-08 10:36:37 +0100

    Add tests for edge cases

--- test/rpn.test.js
+++ test/rpn.test.js
@@ -45,8 +45,26 @@ describe('RPN', function () {
     });
 
     describe('edge cases', function () {
-        it('some test', function () {
-            expect(1).to.equal(1);
+        it("should accept only numbers and operators", function () {
+            expect(evaluateRPN('a 1 +')).to.be.an('Error: expecting only numbers and operators, got `a`');
+        });
+        it('should return 0 when the input is empty', function () {
+            expect(evaluateRPN('')).to.equal('0');
+        });
+        it('should return the number if the input is only one number', function () {
+            expect(evaluateRPN('9')).to.equal('9');
+        });
+        it('should return error if the input contains one number and one operator', function () {
+            expect(evaluateRPN('9 +')).to.be.an('Error: expecting two arguments for `+`, got one (9)');
+        });
+        it('should not accept an operator at first', function () {
+            expect(evaluateRPN('+ 9')).to.be.an('Error: expecting two arguments for `+`, get none ()');
+        });
+        it('should not accept an operator that is not at the end', function () {
+            expect(evaluateRPN('11 + 9')).to.be.an('Error: expecting two arguments for `+`, get one (11)');
+        });
+        it('should return error if there are more numbers than operators + 1', function () {
+            expect(evaluateRPN('9 11 12 +')).to.be.an('Error: operator missing');
         });
     });
 });
