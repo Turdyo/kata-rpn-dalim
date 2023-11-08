05ae1ca956b773eef7bce699465a69951909f166
AbdullatifGhajar <abdullatif.ghajar@gmail.com>
2023-11-08 10:17:37 +0100

    Add test for * and /

--- test/rpn.test.js
+++ test/rpn.test.js
@@ -1,8 +1,5 @@
 const expect = require('chai').expect;
-
-function evaluateRPN(str) {
-    return "";
-}
+const evaluateRPN = require('../src/rpn');
 
 
 describe('RPN', function () {
@@ -23,7 +20,16 @@ describe('RPN', function () {
                 expect(evaluateRPN('9 11 12 - -')).to.equal(-14);
             });
         });
+        describe('should work with *', function () {
+            it('should work with two numbers', function () {
+                expect(evaluateRPN('9 11 *')).to.equal(99);
+            });
+            it('should work with more than two numbers', function () {
+                expect(evaluateRPN('9 11 12 * *')).to.equal(1188);
+            });
+        });
     });
+    
     describe('edge cases', function () {
         it('some test', function () {
             expect(1).to.equal(1);
