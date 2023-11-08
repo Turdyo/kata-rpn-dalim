e5da76d7db156b15b6f38c15fbfb03693054ed56
AbdullatifGhajar <abdullatif.ghajar@gmail.com>
2023-11-08 10:11:18 +0100

    Add tests with only + and -
    Co-authored-by: Stan97-2 <Stan97-2@users.noreply.github.com>
    Co-authored-by: Neith <SwevenNeith@users.noreply.github.com>

--- test/rpn.test.js
+++ test/rpn.test.js
@@ -1,7 +1,30 @@
 const expect = require('chai').expect;
 
+function evaluateRPN(str) {
+    return "";
+}
+
+
 describe('RPN', function () {
-    describe('some category', function () {
+    describe('normal cases', function () {
+        describe('should work with +', function () {
+            it('should work with two numbers', function () {
+                expect(evaluateRPN('9 11 +')).to.equal(20);
+            });
+            it('should work with more than two numbers', function () {
+                expect(evaluateRPN('9 11 12 + +')).to.equal(32);
+            });
+        });
+        describe('should work with -', function () {
+            it('should work with two numbers', function () {
+                expect(evaluateRPN('9 11 -')).to.equal(-2);
+            });
+            it('should work with more than two numbers', function () {
+                expect(evaluateRPN('9 11 12 - -')).to.equal(-14);
+            });
+        });
+    });
+    describe('edge cases', function () {
         it('some test', function () {
             expect(1).to.equal(1);
         });
